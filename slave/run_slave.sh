176e02d2473cf47c9038e074c562c3d46d09a766
m4tland <slave0@slave0.imag.fr>
2015-11-19 19:32:17 +0100

    Debug slave demon with logs - working!

--- slave/run_slave.sh
+++ slave/run_slave.sh
@@ -64,7 +64,8 @@ do
 			#rm previoux job if any
 			docker rm job &>/dev/null	
 			echo "[INFO]- `date` - $file - Starting job execution in Docker" >> $LOG_FILE
-			docker run --name job -v $JOB_ENV_DIR:$CONTAINER_DIR -it -u slave_user $USER /bin/bash "$CONTAINER_CMD &> $CONTAINER_OUTPUT < $CONTAINER_INPUT"
+			docker run --name job -v $JOB_ENV_DIR:$CONTAINER_DIR -it -u slave_user $USER /bin/bash -c "$CONTAINER_CMD &> $CONTAINER_OUTPUT < $CONTAINER_INPUT"
+			#docker run --name job -v $JOB_ENV_DIR:$CONTAINER_DIR -it -u slave_user $USER /bin/bash
 			echo "[INFO]- `date` - $file - End of docker job execution" >> $LOG_FILE
 			
 			#-----------------------
@@ -73,7 +74,8 @@ do
 			echo "[INFO]- `date` - $file - Sending response" >> $LOG_FILE
 			output="$file.output"
 			mv $JOB_ENV_DIR/output $JOB_ENV_DIR/$output
-			scp $JOB_ENV_DIR/$output $ADDRESS_SCHEDULER:$REPOSITORY_SCHEDULER
+			cat $JOB_ENV_DIR/$output
+			scp -v $JOB_ENV_DIR/$output $ADDRESS_SCHEDULER:$REPOSITORY_SCHEDULER
 			
 			#-----------------------
 			# Clean environment
