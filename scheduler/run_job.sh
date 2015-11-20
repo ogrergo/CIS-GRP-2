0880db125c8c9591c96b0c8590f6741161896a91
Luis <lpsilvestrin@inf.ufrgs.br>
2015-11-20 15:39:47 +0100

    Updating some schduler scripts

--- scheduler/run_job.sh
+++ scheduler/run_job.sh
@@ -4,5 +4,6 @@
 echo $1
 echo $2
 username=`tr "@" " " <<< $2 | awk '{ print $1; }'`
+while [[ `lsof 2>/dev/null | grep $1` ]] ; do sleep 2; done
 scp $1 $2:/home/$username/CIS-GRP-2/slave/jobs 2>&1 1>/dev/null
 
